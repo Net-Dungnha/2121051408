e081315bbcc286d654b0eeb64ea8a150bb487706
Your_UserName <dunglynn06012003@gmail.com>
2025-09-24 17:22:34 +0700

    lession 08

--- Lession07/Script.js
+++ Lession07/Script.js
@@ -1,3 +1,3 @@
-document.writeln("Xin chào các bạn đến với khóa học lập trình web tại Freetuts.net");<br>
+document.writeln("Xin chào các bạn đến với khóa học lập trình web tại Freetuts.net");
  let FullName = "Ha Van Dung";
  document.writeln(FullName);
